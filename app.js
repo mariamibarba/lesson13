59ccb32fedf3bb7f19dccdd0ca42037755d4a6cd
mariamibarba <73659597+mariamibarba@users.noreply.github.com>
2022-04-04 15:53:33 +0400

    add html and app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+let userName = "name";
+let userAge = 20;
+let usersecondName;
+let userScore = 99.9;
+let isStudent = true;
+let myNull = null;
+let myNaN = NaN;
+let isEmployed = false;
+
+
+
+
+console.log(userName);
+console.log(userAge);
+console.log(usersecondName);
+console.log(isStudent);
+console.log(myNull);
+console.log(userScore);
+console.log(isEmployed);
+
+
+// ......................................... 
+
+let myName="Mariami"
+let myHobby="Singing"
+let yearOfBirth = 2002
+let currentYear = 2022
+let myAge = currentYear - yearOfBirth
+
+
+
+
+// first way 
+
+console.log(  "My name is" + " " + myName + "," +" "  + "I am" +" " + myAge + " years old and my hobby is " + myHobby + ".")
+
+// second way
+
+let about_mariam = `My name is ${myName}, i am  ${currentYear-yearOfBirth}  years old and my hobby is ${myHobby}`;
+console.log(about_mariam);
