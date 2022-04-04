c26cd8904d5d086106f7696be21618a1b85f3ebd
mariamibarba <73659597+mariamibarba@users.noreply.github.com>
2022-04-04 17:17:21 +0400

    change js

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ let usersecondName;
 let userScore = 99.9;
 let isStudent = true;
 let myNull = null;
+let userNan = NaN;
 let isEmployed = false;
 
 
 
 
-console.log(userName);
-console.log(userAge);
-console.log(usersecondName);
-console.log(isStudent);
-console.log(myNull);
-console.log(userScore);
-console.log(isEmployed);
+// console.log(userName);
+// console.log(userAge);
+// console.log(usersecondName);
+// console.log(isStudent);
+// console.log(myNull);
+// console.log(userScore);
+// console.log(isEmployed);
+// console.log( userNan);
+
 
 
 // ......................................... 
@@ -24,14 +27,14 @@ let myName="Mariami"
 let myHobby="Singing"
 let yearOfBirth = 2002
 let currentYear = 2022
-let myAge = currentYear - yearOfBirth
+// let myAge = currentYear - yearOfBirth
 
 
 
 
 // first way 
 
-console.log(  "My name is" + " " + myName + "," +" "  + "I am" +" " + myAge + " years old and my hobby is " + myHobby + ".")
+console.log(  "My name is" + " " + myName + "," +" "  + "I am" +" " + (currentYear-yearOfBirth) + " years old and my hobby is " + myHobby + ".");
 
 // second way
 
